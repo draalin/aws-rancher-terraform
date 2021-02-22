7231a707967b053732336b84c8ee9e3e55a9a170
Tyler Bailey <tyler@tylerbailey.com>
2021-02-22 07:37:42 -0500

    Update TF providers / versions

--- versions.tf
+++ versions.tf
@@ -1,11 +1,14 @@
 terraform {
-  required_version = ">= 0.13"
+  required_version = ">= 0.14"
   required_providers {
+    aws = {
+      version = "~> 3.2"
+    }
     rke = {
       source = "rancher/rke"
     }
     rancher2 = {
-      source = "terraform-providers/rancher2"
+      source = "rancher/rancher2"
     }
   }
 }
